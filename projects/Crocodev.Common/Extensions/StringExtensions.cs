95aa5b6f2c0089bbbcb58388e08c08f2fc32ea85
Roman Koshelev <romakosh@gmail.com>
2015-03-24 12:29:51 +0300

    restored SafeFormat

--- projects/Crocodev.Common/Extensions/StringExtensions.cs
+++ projects/Crocodev.Common/Extensions/StringExtensions.cs
@@ -1,7 +1,7 @@
-﻿// Crocodev (c) 2015 Crocodev
+﻿// crocodev (c) 2015 Crocodev
 // Crocodev.Common
 // StringExtensions.cs
-// romak_000, 2015-03-19 15:29
+// romak_000, 2015-03-24 12:28
 
 using System;
 using System.Collections.Generic;
@@ -12,16 +12,39 @@ namespace Crocodev.Common.Extensions
     public static class StringExtensions
     {
         // ===================================================================================== []
+        // SafeFormat
+        public static string SafeFormat( this string template, params object[] args )
+        {
+            try {
+                return string.Format( template, args );
+            }
+            catch {
+                // ignored
+            }
+
+            return template;
+        }
+
+        // ===================================================================================== []
+        // TryFormat
+        public static string TryFormat( this string template, params object[] args )
+        {
+            return string.Format( template, args );
+        }
+
+        // ===================================================================================== []
         // FoldToStringBy
         public static string FoldToStringBy<T>(
-            this IEnumerable<T> enumerable,
-            Func<T, string> selector,
+            this IEnumerable< T > enumerable,
+            Func< T, string > selector,
             string delimiter = ", ",
-            string emptyResult = "")
+            string emptyResult = "" )
         {
-            var list = enumerable as IList<T> ?? enumerable.ToList();
-            if (!list.Any()) return emptyResult;
-            return list.Select(selector).Aggregate((a, s) => string.Format("{0}{1}{2}", a, delimiter, s));
+            var list = enumerable as IList< T > ?? enumerable.ToList();
+            if( !list.Any() ) {
+                return emptyResult;
+            }
+            return list.Select( selector ).Aggregate( ( a, s ) => string.Format( "{0}{1}{2}", a, delimiter, s ) );
         }
     }
 }
